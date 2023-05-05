6a6b161048ad0605a5906b21662f94cbc458eae7
Aleksei Erchenko <proger@a1exxuss.ru>
2023-05-05 17:08:14 +0300

    fix: fix output size file kb to byte

--- 03-files-in-folder/index.js
+++ 03-files-in-folder/index.js
@@ -1,27 +1,31 @@
-const fs = require('fs/promises');
+const fsPromise = require('fs/promises');
 const path = require('path');
 
 const pathToDirectory = path.join(__dirname, 'secret-folder');
 
-fs.readdir(pathToDirectory, { withFileTypes: true })
-  .then(array => array.map(direntObj => {
-    return {
-      fileName: direntObj.name,
-      isFile: direntObj.isFile(),
-      extension: path.extname(path.join(__dirname, 'secret-folder', direntObj.name)).replace('.', '')
-    };
-  }))
-  .then(result => result.filter(elem => elem.isFile))
-  .then(stat => {
-    const promises = stat.map(item => {
-      return fs.stat(path.join(__dirname, 'secret-folder', item.fileName))
-        .then(result => {
-          item.size = (result.size / 1024).toFixed(3);
-          return item;
-        });
-    });
-    return Promise.all(promises);
-  })
-  .then(result => result.map(elem => `${elem.fileName.split('.')[0]}-${elem.extension}-${elem.size}Kb`))
-  .then(result => console.log(result))
-  .catch(error => console.log(error.message));
+fsPromise.readdir(pathToDirectory, { withFileTypes: true })
+  .then(
+    array => {
+      const arrayFiles = array.map(direntObj => {
+        return {
+          fileName: direntObj.name,
+          isFile: direntObj.isFile(),
+          extension: path.extname(path.join(__dirname, 'secret-folder', direntObj.name)).replace('.', '')
+        };
+      })
+        .filter(item => item.isFile)
+      const promises = arrayFiles.map(item => {
+        return fsPromise.stat(path.join(__dirname, 'secret-folder', item.fileName))
+          .then(result => {
+            item.size = result.size;
+            return item;
+          })
+          .then(result => result)
+      })
+      return Promise.all(promises);
+    }
+    )
+  .then(
+    result => console.log(result.map(elem => `${elem.fileName.split('.')[0]}-${elem.extension}-${elem.size}`))
+    )
+  .catch(error => console.log(error.message))
